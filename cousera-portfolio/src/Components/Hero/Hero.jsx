07c7cc53fcc1bf608907c62971fd0f35e321537f
Oluwatobimichael <oluwatobiadeyeye.2000@gmail.com>
2024-08-29 01:53:50 +0100

    updated

--- cousera-portfolio/src/Components/Hero/Hero.jsx
+++ cousera-portfolio/src/Components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img} alt="" />
         <h1><span>Hi, I'm Oluwatobi Adeyeye Michael</span>, a graphics designer, 
             ui/ux designer and a frontend developer based in Nigeria
@@ -14,7 +15,7 @@ const Hero = () => {
             tenetur? Est explicabo impedit qui reiciendis facilis et expedita nisi ut iusto
         </p>
         <div className="hero-section">
-            <div className="hero-connect">Connect with me</div>
+            <div className="hero-connect"><AnchorLink className='anchor_link' offset={50} href='#contact'>Connecct With Me</AnchorLink>Connect with me</div>
             <div className="hero-resume">My resume</div>
         </div>
     </div>
